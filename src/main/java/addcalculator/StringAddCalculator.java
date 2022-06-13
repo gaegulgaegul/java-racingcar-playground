8366b691c277297fb633ae1b0bf6fdcb2ad88675
임명섭 <lms@elonsoft.co.kr>
2022-06-13 23:42:25 +0900

    feat 문자열 덧셈 계산기

--- src/main/java/addcalculator/StringAddCalculator.java
+++ src/main/java/addcalculator/StringAddCalculator.java
@@ -1,7 +1,48 @@
 package addcalculator;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
-    public int calculate(String s) {
+
+    private static final String SINGLE_NUMBER_PATTERN = "[0-9]";
+    private static final String DEFAULT_PATTERN = ",|:";
+    private static final String CUSTOM_PATTERN = "//(.)\\n(.*)";
+    private static final String MINUS = "-";
+
+    public int calculate(String text) {
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+
+        if (text.contains(MINUS)) {
+            throw new RuntimeException();
+        }
+
+        if (text.matches(SINGLE_NUMBER_PATTERN)) {
+            return Integer.parseInt(text);
+        }
+
+        if (getMatcher(DEFAULT_PATTERN, text).find()) {
+            String[] split = text.split(DEFAULT_PATTERN);
+            return summary(split);
+        }
+
+        Matcher matcher = getMatcher(CUSTOM_PATTERN, text);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            String[] split = matcher.group(2).split(customDelimiter);
+            return summary(split);
+        }
         return 0;
+    }
+
+    private Matcher getMatcher(String pattern, String text) {
+        return Pattern.compile(pattern).matcher(text);
+    }
+
+    private int summary(String[] split) {
+        return Arrays.stream(split).mapToInt(Integer::parseInt).sum();
     }
 }
